5272f35a03226ed4fc0ddadad9df3bed4b4bf461
Alex Rosario <neoexmachina@gmail.com>
2018-09-23 17:17:46 -0400

    FINALLY update map reading

--- rpg.map.js
+++ rpg.map.js
@@ -154,12 +154,18 @@ Sphere.Map = function(d) {
 		"numZones":0,
 		"reserved":[]
 	};
+	var _str = [];
+	var _lay = [];
+	var _ent = [];
+	var _zones = [];
+	var _ts = null;
 	function _processLayers() {}
 	function _processEntities() {}
 	function _processZones() {}
 	if (d.substr(0,4)===".rmp") {
 		_hdr.version = Sphere.util.parseLE(d.substr(4,2));
 		if (_hdr.version>0) {
+			//// HEADER
 			_hdr.type = d.charCodeAt(6);
 			_hdr.numLayers = d.charCodeAt(7);
 			_hdr.numEntities = Sphere.util.parseLE(d.substr(9,2));
@@ -169,14 +175,163 @@ Sphere.Map = function(d) {
 				"z":d.charCodeAt(15),
 				"direction":d.charCodeAt(16)
 			};
-			_hdr.numStrings = Sphere.util.parseLE(d.substr(17,2));
+			_hdr.numStrings = Sphere.util.parseLE(d.substr(17,2));	// should be 9
 			_hdr.numZones = Sphere.util.parseLE(d.substr(19,2));
 			_hdr.reserved = d.substr(21,235);
+			//// STRINGS
+			var _n = 256;
+			for (var i=0, l=_hdr.numStrings; i<l; ++i) {
+				var _len = Sphere.util.parseLE(d.substr(_n,2));
+				_n += 2;
+				var _s = d.substr(_n,_len);
+				_n += _len;
+				_str.push({
+					"length":_n,
+					"data":_s||""
+				});
+			}
+			//// LAYERS
+			for (var i=0, l=_hdr.numLayers; i<l; ++i) {
+				var _l = {
+					"width":Sphere.util.parseLE(d.substr(_n,2)),
+					"height":Sphere.util.parseLE(d.substr(_n+2,2)),
+					"flags":Sphere.util.parseLE(d.substr(_n+4,2)),
+					"parallax":{
+						"x":Sphere.util.parseFloat32LE(d.substr(_n+6,4)),
+						"y":Sphere.util.parseFloat32LE(d.substr(_n+10,4))
+					},
+					"scroll":{
+						"x":Sphere.util.parseFloat32LE(d.substr(_n+14,4)),
+						"y":Sphere.util.parseFloat32LE(d.substr(_n+18,4))
+					},
+					"numSegments":Sphere.util.parseLE(d.substr(_n+22,4)),
+					"reflective":d.charCodeAt(_n+26),
+					"reserved":Sphere.util.parseLE(d.substr(_n+27,3)),
+					"name":""
+				};
+				_n += 30;
+				_l["name"] = Sphere.util.parseString(d.substr(_n));
+				_n += 2+_l["name"].length;
+				_l["tiles"] = [];
+				var _z = (_l["width"]*_l["height"])<<1;
+				for (var j=0; j<_z; j+=2) {
+					_l["tiles"].push(Sphere.util.parseLE(d.substr(_n,2)));
+					_n += 2;
+				}
+				//_n += _z;
+				_l["obstructions"] = [];
+				for (var j=0, n=_l.numSegments; j<n; ++j) {
+					_l["obstructions"].push({
+						"p1":{
+							"x":Sphere.util.parseLE(d.substr(_n,4)),
+							"y":Sphere.util.parseLE(d.substr(_n+4,4))
+						},
+						"p2":{
+							"x":Sphere.util.parseLE(d.substr(_n+8,4)),
+							"y":Sphere.util.parseLE(d.substr(_n+12,4))
+						}
+					});
+					_n += 16;
+				}
+				_lay.push(_l);
+			}
+			//// ENTITIES
+			for (var i=0, l=_hdr.numEntities; i<l; ++i) {
+				var _e = {
+					"map":{
+						"x":Sphere.util.parseLE(d.substr(_n,2)),
+						"y":Sphere.util.parseLE(d.substr(_n+2,2)),
+						"z":Sphere.util.parseLE(d.substr(_n+4,2))
+					},
+					"type":Sphere.util.parseLE(d.substr(_n+6,2)),
+					"reserved":d.substr(_n+8,8),
+					"data":null
+				};
+				_n += 16;
+				switch (_e.type) {
+					case 1:	// person
+						var _len, _name;
+						_e.data = {
+							"name":"",
+							"spriteset":"",
+							"scripts":[],
+							"reserved":[]
+						};
+						_e.data["name"] = Sphere.util.parseString(d.substr(_n));
+						_n += 2+_e.data["name"].length;
+						_e.data["spriteset"] = Sphere.util.parseString(d.substr(_n));
+						_n += 2+_e.data["spriteset"].length;
+						var _num_scr = Sphere.util.parseLE(d.substr(_n,2));	// should be 5
+						_n += 2;
+						_e.data["numScripts"] = _num_scr;
+						for (var j=0; j<_num_scr; ++j) {
+							var _tmp = Sphere.util.parseString(d.substr(_n));
+							_e.data["scripts"].push(_tmp);
+							_n += 2+_tmp.length;
+						}
+						_e.data["reserved"] = d.substr(_n,16);
+						_n += 16;
+						break;
+					case 2:	// trigger
+						var _len, _name;
+						_e.data = {
+							"func":null
+						};
+						_e.data["func"] = Sphere.util.parseString(d.substr(_n));
+						_n += 2+_e.data["func"].length;
+						break;
+					default:
+						break;
+				}
+				_ent.push(_e);
+			}
+			//// ZONES
+			for (var i=0, l=_hdr.numZones; i<l; ++i) {
+				var _z = {
+					"p1":{
+						"x":Sphere.util.parseLE(d.substr(_n,2)),
+						"y":Sphere.util.parseLE(d.substr(_n+2,2))
+					},
+					"p2":{
+						"x":Sphere.util.parseLE(d.substr(_n+4,2)),
+						"y":Sphere.util.parseLE(d.substr(_n+6,2))
+					},
+					"layer":Sphere.util.parseLE(d.substr(_n+8,2)),
+					"steps":Sphere.util.parseLE(d.substr(_n+10,2)),
+					"reserved":d.substr(_n+12,4)
+				};
+				_n += 16;
+				_z["func"] = Sphere.util.parseString(d.substr(_n));
+				_n += 2+_z["func"].length;
+				_zones.push(_z);
+/****
+---- Zone header ----
+
+Each zone has a 16 byte information header
+word x1;			// starting x-coordinate of entity (pixel coordinates)
+word y1;			// starting y-coordinate of entity (pixel coordinates)
+word x2;			// ending x-coordinate of entity (pixel coordinates)
+word y2;			// ending y-coordinate of entity (pixel coordinates)
+word layer;			// z-coordinate of entity (layer)
+word reactivate_in_num_steps; 	// number of steps required to reactivate
+byte reserved[4];
+
+---- Zone data ----
+
+string function;		// script to be executed when the zone is activated
+
+****/
+			}
 		}
 		else {
 		}
 		_ret = {
-			"header":_hdr
+			"header":_hdr,
+			"strings":_str,
+			"layers":_lay,
+			"entities":_ent,
+			"zones":_zones,
+			"tileset":_ts
 		};
 	}
 	else console.log("Couldn't create map from data");
--- rpg.util.js
+++ rpg.util.js
@@ -26,12 +26,40 @@ Sphere.Enum.Directions = [
 	"northwest"
 ];
 
+//// FROM https://www.codeproject.com/Tips/387989/Convert-Binary-Single-Precision-Value-to-Float-in
+function ieee32ToFloat(intval) {
+	var fval = 0.0;
+	var x;//exponent
+	var m;//mantissa
+	var s;//sign
+	s = (intval & 0x80000000)?-1:1;
+	x = ((intval >> 23) & 0xFF);
+	m = (intval & 0x7FFFFF);
+	switch(x) {
+		case 0:
+			//zero, do nothing, ignore negative zero and subnormals
+			break;
+		case 0xFF:
+			if (m) fval = NaN;
+			else if (s > 0) fval = Number.POSITIVE_INFINITY;
+			else fval = Number.NEGATIVE_INFINITY;
+			break;
+		default:
+			x -= 127;
+			m += 0x800000;
+			fval = s * (m / 8388608.0) * Math.pow(2, x);
+			break;
+	}
+	return fval;
+} 
+
 Sphere.util = {
 	"animate":(function(callback) {
 		return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
 			function(callback) {return window.setTimeout(callback, 1000 / 60);};
 	})(),
 	"parseLE":function(d){var r=0,i=-1;while(++i<d.length)r+=((d.charCodeAt(i)&0xff)<<(8*i));return r;},
+	"parseFloat32LE":function(d){var ret = this.parseLE(d); return ieee32ToFloat(ret);},
 	"parseBitmap":function(d){
 		var _ret = [], q = -1, i = -1; while (i<d.length) {
 			_ret[++q] = Sphere.color(d.charCodeAt(++i)&0xff,d.charCodeAt(++i)&0xff,d.charCodeAt(++i)&0xff,d.charCodeAt(++i)&0xff);
